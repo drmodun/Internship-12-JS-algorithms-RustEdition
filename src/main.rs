0f9fa059311c35c58a6d1a68ecac44a53e82e2fb
drmodun <jan.modun.st@gmail.com>
2023-03-23 21:01:08 +0100

    finished second task (with both filters and for loops)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 
-use std::io::*;
+use std::{io::*, vec};
 use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    task1();
+    //task1();
+    task2();
 }
 
 fn task1(){
@@ -53,3 +56,64 @@ fn task1(){
 
 
 }
+fn task2(){
+    let mut numberOfPeopleTry = String::new();
+    stdin().read_line(&mut numberOfPeopleTry).expect("Failed to read line");
+    let numberOfPeople: i32 = numberOfPeopleTry.trim().parse().expect("Please type a number!");
+    struct Person{
+        name: String,
+        surname: String,
+        height: i32,
+        hairColor: String,
+    }
+    let mut people: Vec<Person> = Vec::new();
+    for i in 0..numberOfPeople {
+        println!("Enter name");
+        let mut name = String::new();
+        stdin().read_line(&mut name).expect("Failed to read line");
+        println!("Enter surname");
+        let mut surname = String::new();
+        stdin().read_line(&mut surname).expect("Failed to read line");
+        println!("Enter height");
+        let mut height = String::new();
+        stdin().read_line(&mut height).expect("Failed to read line");
+        let height: i32 = height.trim().parse().expect("Please type a number!");
+        println!("Enter hair color");
+        let mut hairColor = String::new();
+        stdin().read_line(&mut hairColor).expect("Failed to read line");
+        let person = Person{
+            name: name.trim().to_string(),
+            surname: surname.trim().to_string(),
+            height: height,
+            hairColor: hairColor.trim().to_string(),
+        };
+        people.push(person);
+    }
+    people.sort_by(|a,b| a.height.cmp(&b.height));
+    let middleIndex: usize = numberOfPeople as usize / 2;
+    //let qualifiedPeople = &people[middleIndex..numberOfPeople as usize];
+    let mut usedColors: Vec<String> = Vec::new();
+    let mut maxPeople = 0;
+    let mut maxColor = String::new();
+    for person in middleIndex..numberOfPeople as usize {
+        if usedColors.contains(&people[person].hairColor) {
+            continue
+        }
+        let mut count = 0;
+        usedColors.push(people[person].hairColor.clone());
+        /*for j in middleIndex..numberOfPeople as usize {
+            if people[person].hairColor == people[j].hairColor {
+                count += 1;
+            }
+        }*/
+        let tempClone = &people[middleIndex..numberOfPeople as usize];
+        let sameColor = tempClone.iter().filter(|&x| x.hairColor == people[person].hairColor).count();
+        if sameColor > maxPeople {
+            maxPeople = sameColor;
+            maxColor = people[person].hairColor.clone();
+            println!("{} {}", maxPeople, maxColor)
+        }
+    }
+    println!("Most popular color is {} and it is used by {} people", maxColor, maxPeople);
+        
+    }  
